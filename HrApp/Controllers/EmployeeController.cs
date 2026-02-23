22423a1783583e910c6deecd362282853f58fd64
Tolga <11307703@student.pxl.be>
2026-02-23 19:09:21 +0100

    EmployeeRepository en EmployeeController afgewerkt

--- HrApp/Controllers/EmployeeController.cs
+++ HrApp/Controllers/EmployeeController.cs
@@ -27,23 +27,21 @@ namespace HrApp.Controllers
             return View(list);
         }
 
-        //// GET: Employee/Details/5
-        //public async Task<IActionResult> Details(int? id)
-        //{
-        //    if (id == null || _context.Employees == null)
-        //    {
-        //        return NotFound();
-        //    }
+        // GET: Employee/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _employeeRepository.GetAll() == null)
+            {
+                return NotFound();
+            }
 
-        //    var employee = await _context.Employees
-        //        .FirstOrDefaultAsync(m => m.EmployeeId == id);
-        //    if (employee == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(employee);
-        //}
+            var employee = await _employeeRepository.GetById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
 
         // GET: Employee/Create
         public IActionResult Create()
@@ -60,103 +58,84 @@ namespace HrApp.Controllers
         {
             if (ModelState.IsValid)
             {
-                _employeeRepository.Add(employee);
+                await _employeeRepository.Add(employee);
                 return RedirectToAction(nameof(Index));
             }
             return View(employee);
         }
 
         //// GET: Employee/Edit/5
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id == null || _context.Employees == null)
-        //    {
-        //        return NotFound();
-        //    }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _employeeRepository.GetAll() == null)
+            {
+                return NotFound();
+            }
 
-        //    var employee = await _context.Employees.FindAsync(id);
-        //    if (employee == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(employee);
-        //}
+            var employee = await _employeeRepository.GetById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return View(employee);
+        }
 
         //// POST: Employee/Edit/5
         //// To protect from overposting attacks, enable the specific properties you want to bind to.
         //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("EmployeeId,FirstName,LastName")] Employee employee)
-        //{
-        //    if (id != employee.EmployeeId)
-        //    {
-        //        return NotFound();
-        //    }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EmployeeId,FirstName,LastName")] Employee employee)
+        {
+            if (id != employee.EmployeeId)
+            {
+                return NotFound();
+            }
 
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(employee);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!EmployeeExists(employee.EmployeeId))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(employee);
-        //}
+            if (ModelState.IsValid)
+            {
 
-        //// GET: Employee/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null || _context.Employees == null)
-        //    {
-        //        return NotFound();
-        //    }
+                await _employeeRepository.Update(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                ModelState.AddModelError("","Gebruiker kon niet geupdatet worden");
+            }
+            return View(employee);
+        }
 
-        //    var employee = await _context.Employees
-        //        .FirstOrDefaultAsync(m => m.EmployeeId == id);
-        //    if (employee == null)
-        //    {
-        //        return NotFound();
-        //    }
+        // GET: Employee/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _employeeRepository.GetAll() == null)
+            {
+                return NotFound();
+            }
 
-        //    return View(employee);
-        //}
+            var employee = await _employeeRepository.GetById(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
 
         //// POST: Employee/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    if (_context.Employees == null)
-        //    {
-        //        return Problem("Entity set 'AppDbContext.Employee'  is null.");
-        //    }
-        //    var employee = await _context.Employees.FindAsync(id);
-        //    if (employee != null)
-        //    {
-        //        _context.Employees.Remove(employee);
-        //    }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var employee = await _employeeRepository.GetById(id);
 
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
+            if (employee == null)
+                return NotFound();
 
-        //private bool EmployeeExists(int id)
-        //{
-        //  return (_context.Employees?.Any(e => e.EmployeeId == id)).GetValueOrDefault();
-        //}
+            await _employeeRepository.Delete(employee);
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
